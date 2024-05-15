c759451b526b7903c757d3893f4a7c64559d2c12
Volodymyr Vasylchenko <vova19007@gmail.com>
2024-05-15 15:51:25 +0300

    Core logic
    - added interface wrapper type (runtime)
    - added basic drawer with additional validation (editor)

---
+++ Runtime/InterfaceReference.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Fofanius.Type
+{
+    [Serializable]
+    public class InterfaceReference<T> where T : class
+    {
+        [SerializeField] private Object _object;
+
+        public bool IsEmpty => !_object;
+
+        public T GetValue()
+        {
+            return _object switch
+            {
+                Component component => component as T,
+                ScriptableObject scriptableObject => scriptableObject as T,
+                _ => default
+            };
+        }
+
+        public override string ToString() => $"({typeof(T).Name}) {(IsEmpty ? "NULL" : _object)}";
+    }
+}
